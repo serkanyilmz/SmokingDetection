3b07595e494c160347fbe2f62333ff2b2cdd6a23
silasahin03 <silasahin2303@gmail.com>
2025-12-10 21:12:14 +0300

    main

--- dataset1 Smoking/main.py
+++ dataset1 Smoking/main.py
@@ -1,7 +1,18 @@
+from preprocess import encode_and_convert_data
+from common.normalization import normalize_dataset
+from common.read_dataset import read_dataset
+from common.methods.decision_tree import decision_tree
+from common.methods.knn import knn
+from common.methods.logistic_regression import logistic_regression
+from common.methods.svm import linear_svm
+from common.methods.naive_bayes import naive_bayes
 
-data=datasetioku
 
-data = preprocess(data)
+data = read_dataset("smoking.csv")
+# data bölme
+proced_data = encode_and_convert_data(data)
+normalized_data = normalize_dataset(proced_data)
+
 
 acc pred = decision_tree(data)
 show_result(acc, red)
