e64ec55d110d07487071525a4132e61f7debfe9b
Serkan Yilmaz <serkan.2003.yilmaz@gmail.com>
2025-12-12 00:16:17 +0300

    merge

--- dataset1 Smoking/main.py
+++ dataset1 Smoking/main.py
@@ -1,12 +1,21 @@
+from common.methods.knn import knn_predict
 from common.methods.logistic_regression import logistic_regression
 from preprocess import encode_and_convert_data
 from common.normalization import normalize_dataset
 from common.methods.naive_bayes import naive_bayes
 from common.read_dataset import load_csv
+from common.methods.naive_bayes import naive_bayes
+from common.read_dataset import load_csv
+from common.methods.decision_tree import decision_tree
+
 
 dataset = load_csv("smoking.csv")
 dataset = encode_and_convert_data(dataset)
 dataset = normalize_dataset(dataset)
 
-#naive_bayes(dataset)
+dataset = dataset[:200]
+
 logistic_regression(dataset)
+naive_bayes(dataset)
+decision_tree(dataset)
+knn_predict(dataset)
