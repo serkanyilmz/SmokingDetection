5f3cf2bc3657ae739847a8ba2700051e6949775d
silasahin03 <silasahin2303@gmail.com>
2025-12-11 18:38:33 +0300

    main dosyalarına knn ve decision tree eklendi

--- dataset1 Smoking/main.py
+++ dataset1 Smoking/main.py
@@ -1,11 +1,14 @@
 from preprocess import encode_and_convert_data
 from common.normalization import normalize_dataset
-from common.methods.naive_bayes import naive_bayes
 from common.read_dataset import load_csv
+from common.methods.knn import knn
+from common.methods.naive_bayes import naive_bayes
+from common.methods.decision_tree import decision_tree
 
 dataset = load_csv("smoking.csv")
 dataset = encode_and_convert_data(dataset)
 dataset = normalize_dataset(dataset)
 
 naive_bayes(dataset)
-
+knn(dataset)
+decision_tree(dataset)
--- dataset2 Smoking Drinking/main.py
+++ dataset2 Smoking Drinking/main.py
@@ -1,3 +1,5 @@
+from common.methods.decision_tree import decision_tree
+from common.methods.knn import knn
 from common.methods.naive_bayes import naive_bayes
 from common.normalization import normalize_dataset
 from common.read_dataset import load_csv
@@ -9,6 +11,8 @@ dataset = encode_and_convert_data(dataset)
 dataset = normalize_dataset(dataset)
 
 naive_bayes(dataset)
+knn(dataset)
+decision_tree(dataset)
 
 #data=datasetioku
 #
